f72ec90abfb9f01c45d2d621fd3d0e0d71fc68a1
kaseymeier <kaseymeier588@gmail.com>
2023-11-15 10:14:14 -0500

    y

--- game.py
+++ game.py
@@ -11,17 +11,22 @@ floor = pygame.image.load("assets/images/floor.png").convert()
 background = screen.copy()
 clock = pygame.time.Clock()
 def drawbackground():
-     #ceiling
-	for i in range(SCREEN_WIDTH // TILE_SIZE):
-		background.blit(wall, (TILE_SIZE * i,0))
-    #floor
-for i in range (SCREEN_WIDTH // TILE_SIZE):
+    #bottom
+    for i in range (SCREEN_WIDTH // TILE_SIZE):
         background.blit(wall,(TILE_SIZE *i,SCREEN_HEIGHT-16 ))
     #left side
 for i in range (SCREEN_HEIGHT// TILE_SIZE):
     background.blit(wall, (0, TILE_SIZE * i))
     #right side
-    
+for i in range (SCREEN_HEIGHT //TILE_SIZE):
+    background.blit(wall, (SCREEN_WIDTH-16, TILE_SIZE* i))
+    # ceiling
+    for i in range(SCREEN_WIDTH // TILE_SIZE):
+        background.blit(wall, (TILE_SIZE * i, 0))
+    #floor
+[background.blit(floor, (SCREEN_WIDTH - (x * TILE_SIZE), (TILE_SIZE * i + (2 * TILE_SIZE)))) for x in range(SCREEN_WIDTH-2*TILE_SIZE)) for i
+ in range(SCREEN_WIDTH // TILE_SIZE)]
+
 
 
 drawbackground()
